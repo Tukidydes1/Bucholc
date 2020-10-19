1a9e6fd984ddfa948ee19e835054d996baeb0bdc
Dominik Zalewski <dominik92ck@gmail.com>
2020-10-19 19:20:45 +0200

    Dodaj pliki projektów.

---
+++ BucholcModularLibrary/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BucholcModularLibrary.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Routing;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Display;
+using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Records;
+using OrchardCore.DisplayManagement;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Notify;
+using OrchardCore.Navigation;
+using OrchardCore.Settings;
+using YesSql;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BucholcModularLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IContentManager _contentManager;
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly IContentItemDisplayManager _contentItemDisplayManager;
+        private readonly IHtmlLocalizer H;
+        private readonly dynamic New;
+        private readonly INotifier _notifier;
+        private readonly ISession _session;
+        private readonly ISiteService _siteService;
+        private readonly IUpdateModelAccessor _updateModelAccessor;
+
+        public HomeController(IContentManager contentManager, IContentDefinitionManager contentDefinitionManager, IContentItemDisplayManager contentItemDisplayManager, IHtmlLocalizer<HomeController> htmlLocalizer, INotifier notifier, ISession session, IShapeFactory shapeFactory, ISiteService siteService, IUpdateModelAccessor updateModelAccessor)
+        {
+            _contentManager = contentManager;
+            _contentDefinitionManager = contentDefinitionManager;
+            _contentItemDisplayManager = contentItemDisplayManager;
+            _notifier = notifier;
+            _session = session;
+            _siteService = siteService;
+            _updateModelAccessor = updateModelAccessor;
+
+            H = htmlLocalizer;
+            New = shapeFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BucholcModularLibrary/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "BucholcModularLibrary",
+    Author = "Marcin Kirylak & Dominik Zalewski",
+    Version = "1.0.0-rc2",
+    Description = "BucholcModularLibrary",
+    Category = "Content Management"
+)]
